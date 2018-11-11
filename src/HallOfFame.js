1874c9c6b4ab2cf48797f828a10e62ad961f8e5f
Antho <antho.oclock@gmail.com>
2018-11-11 13:37:36 +0100

    update HallOfFame.js

--- src/HallOfFame.js
+++ src/HallOfFame.js
@@ -40,3 +40,27 @@ export const FAKE_HOF = [
   { id: 0, guesses: 38, date: '14/10/2018', player: 'Puppy' },
 ]
 
+const HOF_KEY = '::Memory::HallofFame'
+const HOF_MAX_SIZE = 10
+
+export function saveHOFEntry(entry, onStored) {
+  entry.date = new Date().toLocaleDateString()
+  entry.id = Date.now()
+
+  const entries = JSON.parse(localStorage.getItem(HOF_KEY) || '[]')
+  const insertionPoint = entries.findIndex(
+    ({ guesses }) => guesses >= entry.guesses
+  )
+
+  if (insertionPoint === -1) {
+    entries.push(entry)
+  } else {
+    entries.splice(insertionPoint, 0, entry)
+  }
+  if (entries.length > HOF_MAX_SIZE) {
+    entries.splice(HOF_MAX_SIZE, entries.length)
+  }
+
+  localStorage.setItem(HOF_KEY, JSON.stringify(entries))
+  onStored(entries)
+}
